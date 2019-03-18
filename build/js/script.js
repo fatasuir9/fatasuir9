e8bd84a3301914638e3f646aae9afb3628483da7
Ghis <fata.suir9@gmail.com>
2019-03-18 22:27:00 +0800

    Design a day mode icon to be activated when night mode is on

--- build/js/script.js
+++ build/js/script.js
@@ -17,13 +17,21 @@ $(document).ready(function() {
 	$('#dark').click(function() {
 		if($('html').hasClass('dark') == true) {
 			$('html').removeClass('dark');
+			$('#dark img').attr('src', '../../images/icons/icon-night.svg');
 			$.cookie('darkmode', '0', {path: '/'});
 		}
 		else {
 			$('html').addClass('dark');
+			$('#dark img').attr('src', '../../images/icons/icon-day.svg');
 			$.cookie('darkmode', '1', {path: '/'});
 		}
 	});
+
+	if($('html').hasClass('dark') == true) {
+		$('#dark img').attr('src', '../../images/icons/icon-day.svg');
+	} else {
+		$('#dark img').attr('src', '../../images/icons/icon-night.svg');
+	}
 
 });
 
